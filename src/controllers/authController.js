e820e610ffec4e265398774bae566351752fb2b4
Marie <mje.espinosa@gmail.com>
2024-11-24 17:05:37 +0100

    add login route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const UserModel = require('../models/userModel');
 const { verifyUser } = require('../validators/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     register: async (req, res) => {
@@ -39,5 +40,44 @@ module.exports = {
                 message: error.message || 'Some error occurred while registering user'
             });
         }
+    },
+    login: async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            const user = await UserModel.findOne({ email });
+            if (!user) {
+                return res.status(401).send({ message: 'Email not found' });
+            }
+
+            const isPasswordCorrect = await bcrypt.compare(password, user.password);
+            if (!isPasswordCorrect) {
+                return res.status(401).send({ message: 'Email or Password wrong' });
+            }
+
+            const userData = {
+                email: user.email
+            };
+            const secret = process.env.JWT_SECRET || 'secret';
+            const jwtData = {
+                expiresIn: process.env.JWT_TIMEOUT_DURATION || '1h'
+            };
+
+            const token = jwt.sign(userData, secret, jwtData);
+
+            res.status(200).send({
+                message: 'Successfully logged in',
+                user: {
+                    firstname: user.firstName,
+                    lastname: user.lastName,
+                    email: user.email,
+                    token
+                }
+            });
+        } catch (error) {
+            res.status(500).send({
+                message: error.message || 'some error occurred while logging user'
+            });
+        }
     }
+
 };
